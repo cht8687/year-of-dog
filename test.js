498ef6ae277e65ea080cd5ff41bb28c592bf7bc3
cht8687 <cht8687@gmail.com>
2016-09-17 13:05:54 +1000

    fix test

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ var test = require('tape');
 
 test('it should check if a year is year of rooster', function(t) {
   t.plan(4);
-  t.equal(yearOfRooster(1992), true, "1992 is year of rooster");
-  t.equal(yearOfRooster(1993), false, "1993 is not year of rooster");
-  t.equal(yearOfRooster(new Date(2005, 0)), true, "2003 is year of rooster");
+  t.equal(yearOfRooster(1993), true, "1993 is year of rooster");
+  t.equal(yearOfRooster(1994), false, "1994 is not year of rooster");
+  t.equal(yearOfRooster(new Date(2005, 0)), true, "2005 is year of rooster");
   t.equal(typeof yearOfRooster() === 'boolean', true);
 });
